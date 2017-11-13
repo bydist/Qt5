47b005341399d3f78b7f2dddee09bce9a354cff1
David Callu <callu.david@gmail.com>
2017-11-13 21:19:25 +0100

    feat(option): remove option/dependencies.cmake, and dispatch in per platform file

--- options/apple.cmake
+++ options/apple.cmake
@@ -1,6 +1,15 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/_unix.cmake")
 
+byd__Qt5__configure__add_args(${package} -system-zlib)
+byd__Qt5__configure__add_args(${package} -system-libpng)
+byd__Qt5__configure__add_args(${package} -system-libjpeg)
+byd__Qt5__configure__add_args(${package} -qt-doubleconversion)
+byd__Qt5__configure__add_args(${package} -qt-freetype)
+byd__Qt5__configure__add_args(${package} -qt-harfbuzz)
+byd__Qt5__configure__add_args(${package} -openssl)
+byd__Qt5__configure__add_args(${package} -libproxy)
+byd__Qt5__configure__add_args(${package} -qt-pcre)
 
 byd__Qt5__configure__add_args(${package} -no-icu)
 
--- options/msvc.cmake
+++ options/msvc.cmake
@@ -1,4 +1,14 @@
 
+byd__Qt5__configure__add_args(${package} -system-zlib)
+byd__Qt5__configure__add_args(${package} -system-libpng)
+byd__Qt5__configure__add_args(${package} -system-libjpeg)
+byd__Qt5__configure__add_args(${package} -qt-doubleconversion)
+byd__Qt5__configure__add_args(${package} -qt-freetype)
+byd__Qt5__configure__add_args(${package} -qt-harfbuzz)
+byd__Qt5__configure__add_args(${package} -openssl)
+byd__Qt5__configure__add_args(${package} -libproxy)
+byd__Qt5__configure__add_args(${package} -qt-pcre)
+
 byd__Qt5__configure__add_args(${package} -directwrite)
 byd__Qt5__configure__add_args(${package} -opengl dynamic)
 byd__Qt5__configure__add_args(${package} -audio-backend)
