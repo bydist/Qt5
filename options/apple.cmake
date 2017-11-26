a0753ab03c7dfad30e1793be674b03cd76cccc64
David Callu <callu.david@gmail.com>
2017-11-26 18:13:08 +0100

    refactor(option): move get_osx_sdk_short_name in cmut

--- options/apple.cmake
+++ options/apple.cmake
@@ -25,12 +25,7 @@ byd__Qt5__configure__add_args(${package} -securetransport)
 
 if(CMAKE_OSX_SYSROOT)
 
-    # workaround : cmake 3.5.2 convert CMAKE_OSX_SYSROOT short name to SDK path
-    # retrieve short name from path
-    get_filename_component(SDKROOT_FOR_${module} ${CMAKE_OSX_SYSROOT} NAME)
-    string(TOLOWER ${SDKROOT_FOR_${module}} SDKROOT_FOR_${module})
-    string(REGEX MATCH "[a-z]+[0-9]+\.[0-9]+" SDKROOT_FOR_${module} ${SDKROOT_FOR_${module}})
-
+    cmut__system__get_osx_sdk_short_name(SDKROOT_FOR_${module})
     byd__Qt5__configure__add_args(${package} -sdk ${SDKROOT_FOR_${module}})
 
 endif()
