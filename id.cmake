72d537420baf84c8792ef4e48ab3112e8523548b
David Callu <callu.david@gmail.com>
2018-07-04 10:15:07 +0200

    feat(patch): disable werror when build qtwebengine/chromium/v8

--- id.cmake
+++ id.cmake
@@ -4,7 +4,7 @@ set(version 5.7.1)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-14
+    VERSION ${version}-15
     ABI ${version}
     )
 
