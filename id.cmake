5e5522f044bea81196cdd2c646c9f76fa55e21a8
David Callu <callu.david@gmail.com>
2019-04-12 16:02:59 +0200

    chore(patch): fix debug release version

--- id.cmake
+++ id.cmake
@@ -4,7 +4,7 @@ set(version 5.12.2)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-2
+    VERSION ${version}-3
     ABI 5.12
     )
 
