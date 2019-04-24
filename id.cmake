3565f0c98e1e604f80ee9c0e649c40c7889f505e
David Callu <callu.david@gmail.com>
2019-04-24 16:23:44 +0200

    chore(id): bump to 5.12.3

--- id.cmake
+++ id.cmake
@@ -1,10 +1,10 @@
 set(package Qt5)
-set(version 5.12.2)
+set(version 5.12.3)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-3
+    VERSION ${version}-1
     ABI 5.12
     )
 
@@ -12,5 +12,5 @@ string(REGEX MATCH "[0-9]+\.[0-9]+" version_major_minor ${version})
 
 byd__package__download_info(${package}
     URL "http://download.qt.io/official_releases/qt/${version_major_minor}/${version}/single/qt-everywhere-src-${version}.tar.xz"
-    URL_HASH SHA256=59b8cb4e728450b21224dcaaa40eb25bafc5196b6988f2225c394c6b7f881ff5
+    URL_HASH SHA256=6462ac74c00ff466487d8ef8d0922971aa5b1d5b33c0753308ec9d57711f5a42
 )
